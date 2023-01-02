b8dfd1e1af5b0e0ce8b18595eb98ccd093276cb3
Viktar Berazouski <berezovskij.work@gmail.com>
2023-01-02 15:43:18 +0300

    HW 5 Домашнее задание – Валидация данных

--- Models/Commodity.cs
+++ Models/Commodity.cs
@@ -10,14 +10,19 @@ namespace LeanProd.Models
         [NotMapped]
         public List<Commodity> Commodities { get; }
        
-        public int Id { get; set; }
-       
         [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Не указано имя")]
+        [StringLength(150, MinimumLength = 3)]
         public string Name { get; set; }
        
+        [StringLength(500, MinimumLength = 0)]
         public string Description { get; set; }
 
-
+        [Required(ErrorMessage = "Не указан товарный код")]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "Код должен содержать только цифры")]
+        [StringLength(13, MinimumLength = 13, ErrorMessage = "Длина кода должна составлять 13 символов")]
         public string EAN13 { get; set; }
 
         
